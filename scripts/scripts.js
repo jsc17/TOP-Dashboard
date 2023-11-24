81c5ecf052314f7ec1ed5ee9c45e77a3457f7bbf
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2023-11-23 22:27:57 -0500

    Add theme colors and change button javascript

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+const themeButton = document.querySelector("#theme-button");
+const root = document.documentElement;
+
+themeButton.addEventListener("click", function () {
+  if (root.classList.contains("light")) {
+    root.classList.remove("light");
+    root.classList.add("dark");
+  } else {
+    root.classList.remove("dark");
+    root.classList.add("light");
+  }
+});
