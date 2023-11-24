4e45b116cff6ffce4edaee013323b1a2e7b3692f
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2023-11-23 22:57:57 -0500

    Add filters to additional icons and adjust theme

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,21 @@
 const themeButton = document.querySelector("#theme-button");
 const root = document.documentElement;
+const headerIcons = document.querySelectorAll(".filter-light");
 
 themeButton.addEventListener("click", function () {
   if (root.classList.contains("light")) {
     root.classList.remove("light");
     root.classList.add("dark");
+    headerIcons.forEach((icon) => {
+      icon.classList.remove("filter-light");
+      icon.classList.add("filter-dark");
+    });
   } else {
     root.classList.remove("dark");
     root.classList.add("light");
+    headerIcons.forEach((icon) => {
+      icon.classList.remove("filter-dark");
+      icon.classList.add("filter-light");
+    });
   }
 });
